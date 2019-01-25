799b130bc3bcfac9692fe3355d62b0aac9f1ecdc
styl <stchatzim@gmail.com>
2019-01-25 10:17:58 +0200

    first commit , setting up eclipse project

---
+++ src/main/java/gr/manolis/steli/markets/App.java
@@ -0,0 +1,13 @@
+package gr.manolis.steli.markets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
