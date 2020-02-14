a076df283065d990674af91a1e24a6a5d40517c5
Stylian <stylianos.chatzimanolis@dotmatics.com>
2020-02-14 16:22:04 +0200

    reworked project in basic test example

---
+++ src/main/java/gr/manolis/steli/markets/CalcExample.java
@@ -0,0 +1,13 @@
+package gr.manolis.steli.markets;
+
+public class CalcExample {
+
+    public double add(double... numbers) {
+        double sum = 0;
+        for(double num: numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+}
---
+++ src/test/java/gr/manolis/steli/markets/CalcExampleTest.java
@@ -0,0 +1,19 @@
+package gr.manolis.steli.markets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalcExampleTest {
+
+    @Test
+    public void testAdd() {
+        CalcExample calc = new CalcExample();
+        double delta = .1;
+
+        assertEquals(15, calc.add(5, 7, 3), delta);
+        assertEquals(8, calc.add(5, 3), delta);
+
+    }
+
+}
