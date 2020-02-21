e93cde1ff85d52a523763cd7baf9f976b21a8b9c
Stylian <stylianos.chatzimanolis@dotmatics.com>
2020-02-21 15:59:02 +0200

    reverted

--- src/test/java/gr/manolis/steli/markets/CalcExampleTest.java
+++ src/test/java/gr/manolis/steli/markets/CalcExampleTest.java
@@ -16,14 +16,4 @@ public class CalcExampleTest {
 
     }
 
-    @Test
-    public void testFailMe() {
-        CalcExample calc = new CalcExample();
-        double delta = .1;
-
-        assertEquals(15, calc.add(5, 9, 3), delta);
-        assertEquals(8, calc.add(5, 3), delta);
-
-    }
-
 }
