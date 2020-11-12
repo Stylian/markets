deb1313e77072d1ffeb5c18b5f74a6f198456019
Stylianos <stylianos.chatzimanolis@dotmatics.com>
2020-11-12 13:56:09 +0200

    added axios added get and post tests

--- ui/src/features/TestPage/TestPage.jsx
+++ ui/src/features/TestPage/TestPage.jsx
@@ -1,20 +1,33 @@
 import {useDispatch, useSelector} from "react-redux";
-import React from "react";
+import React, {useEffect} from "react";
 import styles from './TestPage.module.css';
 import {
-    postTest, selectRuns,
+    getTests,
+    postTest,
+    selectIsLoaded,
+    selectRuns,
+    selectTests,
 } from './testPageSlice';
+import {loadBuyingClients, loadSellingClients} from "../TradeCenter/tradeCenterSlice";
 
 export function TestPage() {
     const dispatch = useDispatch();
+    const isLoaded = useSelector(selectIsLoaded);
     const runningTimes = useSelector(selectRuns);
-
-    return (
+    const tests = useSelector(selectTests);
+    
+    useEffect( () => {
+        dispatch(getTests())
+    }, [dispatch]);
+    
+    return isLoaded ? (
         <div className={styles.main}>
+            {/*{tests.map(test => <p>{test}</p>)}*/}
+            {tests[0].counter}
             <button
                 onClick={() => dispatch(postTest())}
             > Test </button>
             {runningTimes}
         </div>
-    );
+    ) : (null);
 }
--- ui/src/features/TestPage/testPageSlice.jsx
+++ ui/src/features/TestPage/testPageSlice.jsx
@@ -1,40 +1,56 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from 'axios';
 
 
-export const postTest = createAsyncThunk('/testPage/addTest', async () => {
-    return fetch("/test/",
+export const getTests = createAsyncThunk('/testPage/getTests', () =>
+    axios.get("/test/",)
+        .then(response => response.data)
+        .catch(error => error)
+);
+
+export const postTest = createAsyncThunk('/testPage/addTest', () => 
+    axios.post("/test/",
         {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            // body: this.props.match.params.seasonNum 
         })
-        .then(res => res.json())
-        .then(
-            (result) => {
-                // state.hasRun += 1;
-            },
-            (error) => {
-            }
-        )
-})
+        .then(response => response.data)
+        .catch(error => error)
+);
 
 export const testPageSlice = createSlice({
     name: 'testPage',
     initialState: {
+        isLoaded: false,
         hasRun: 0,
+        tests: [],
     },
     reducers: {
     },
     extraReducers: {
+        [getTests.pending]: (state, action) => {
+            console.log("pending");
+
+        },
+        [getTests.fulfilled]: (state, action) => {
+            console.log("fulfilled");
+            console.log(action.payload)
+            state.tests = action.payload;
+            state.isLoaded = true;
+        },
         [postTest.pending]: (state, action) => {
             console.log("pending");
+
         },
         [postTest.fulfilled]: (state, action) => {
             console.log("fulfilled");
+            console.log(action.payload)
+            state.hasRun += 1;
         }
     }
 });
 
+export const selectIsLoaded = state => state.testPage.isLoaded;
 export const selectRuns = state => state.testPage.hasRun;
+export const selectTests = state => state.testPage.tests;
+
 
 export default testPageSlice.reducer;
