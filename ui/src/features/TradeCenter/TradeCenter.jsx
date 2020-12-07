08f5c31a45281742d1d7c42774ef10db66ab0540
Stylianos <stylianos.chatzimanolis@dotmatics.com>
2020-12-07 11:11:58 +0200

    rework on trade center page

--- ui/src/features/TradeCenter/TradeCenter.jsx
+++ ui/src/features/TradeCenter/TradeCenter.jsx
@@ -1,12 +1,7 @@
 import React, {useEffect} from "react";
 import styles from './TradeCenter.module.css';
-import {
-    loadBuyingClients,
-    selectBuyingClients,
-    loadSellingClients,
-    selectSellingClients
-} from "./tradeCenterSlice";
 import {useDispatch, useSelector} from "react-redux";
+import {loadTradeCenter} from "./tradeCenterSlice";
 
 
 const Tableheaders = (props) => {
@@ -32,7 +27,7 @@ const TableRows = (props) => {
             return (
                 <tr>
                     <td>{index + 1}</td>
-                    <td>{row.node}</td>
+                    <td>{row.client.name}</td>
                     <td>{row.price}</td>
                     <td>{row.quantity}</td>
                 </tr>
@@ -43,30 +38,31 @@ const TableRows = (props) => {
 }
 
 export function TradeCenter() {
-    const buyingClients = useSelector(selectBuyingClients);
-    const sellingClients = useSelector(selectSellingClients);
     const dispatch = useDispatch();
+    const isLoaded = useSelector(state => state.tradeCenter.isLoaded);
+    const tradeCenter = useSelector(state => state.tradeCenter.trade_center_obj);
 
     useEffect( () => {
-        dispatch(loadBuyingClients())
-        dispatch(loadSellingClients())
+        dispatch(loadTradeCenter());
     }, [dispatch]);
     
-    return (
+    let timber = isLoaded? tradeCenter.tradeTables['TIMBER'] : null;
+    
+    return isLoaded ? (
         <div className={styles.splitscreen}>
             <div className={styles.left}>
                 <table>
                     <Tableheaders type={'buy'} />
-                    <TableRows rows={buyingClients} />
+                    <TableRows rows={timber.buyingOffers} />
                 </table>
             </div>
 
             <div className={styles.right}>
                 <table>
                     <Tableheaders type={'sell'} />
-                    <TableRows rows={sellingClients} />
+                    <TableRows rows={timber.sellingOffers} />
                 </table>
             </div>
         </div>
-    );
+    ) : (null);
 }
--- ui/src/features/TradeCenter/tradeCenterSlice.jsx
+++ ui/src/features/TradeCenter/tradeCenterSlice.jsx
@@ -1,65 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from "axios";
+
+export const loadTradeCenter = createAsyncThunk('/trade_center', () =>
+    axios.get("/api/trade_center/",
+        {
+        })
+        .then(response => response.data)
+        .catch(error => error)
+);
 
 export const tradeCenterSlice = createSlice({
     name: 'tradeCenter',
     initialState: {
-        buyingClients: [],
-        sellingClients: [],
+        trade_center_obj: null,
+        isLoaded: false,
     },
     reducers: {
-        loadBuyingClients: (state, action) => {
-            state.buyingClients = [
-                {
-                    node: 'Venice',
-                    price: 28,
-                    quantity: 2000
-                },
-                {
-                    node: 'Marseille',
-                    price: 27,
-                    quantity: 1500
-                },
-                {
-                    node: 'Tunis',
-                    price: 27,
-                    quantity: 300
-                },
-                {
-                    node: 'Genoa',
-                    price: 25,
-                    quantity: 400
-                },
-                {
-                    node: 'Lisbon',
-                    price: 23,
-                    quantity: 600
-                },
-            ];
+    },
+    extraReducers: {
+        [loadTradeCenter.pending]: (state, action) => {
+            console.log("loadTradeCenter pending");
+
         },
-        loadSellingClients: (state, action) => {
-            state.sellingClients = [
-                {
-                    node: 'Alexandria',
-                    price: 24,
-                    quantity: 800
-                },
-                {
-                    node: 'Antioch',
-                    price: 25,
-                    quantity: 900
-                },
-                {
-                    node: 'Crimea',
-                    price: 29,
-                    quantity: 400
-                },
-            ];
+        [loadTradeCenter.fulfilled]: (state, action) => {
+            console.log("loadTradeCenter fulfilled");
+            state.trade_center_obj = action.payload;
+            state.isLoaded = true;
         },
     },
 });
-
-export const { loadBuyingClients, loadSellingClients } = tradeCenterSlice.actions;
-export const selectBuyingClients = state => state.tradeCenter.buyingClients;
-export const selectSellingClients = state => state.tradeCenter.sellingClients;
 
 export default tradeCenterSlice.reducer;
